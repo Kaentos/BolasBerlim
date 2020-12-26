dd4e9d287aa5f8335cb633d7f5592f48285841fe
Tomás Matos <58147144+TomasKuki@users.noreply.github.com>
2020-12-26 23:07:59 +0000

    pop ups

---
+++ js/hiddenContent.js
@@ -0,0 +1,18 @@
+function profileMenuFun() {
+
+    var content = document.getElementById("profileMenu");
+    if (content.style.display === "none") {
+        content.style.display = "flex";
+    } else {
+        content.style.display = "none";
+    }
+  }
+  function notificacaoMenuFun() {
+
+    var content = document.getElementById("notificacoesMenu");
+    if (content.style.display === "none") {
+        content.style.display = "flex";
+    } else {
+        content.style.display = "none";
+    }
+  }
